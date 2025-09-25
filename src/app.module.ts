fce7dbb80eb5d6e741bfba72a53168851fcab759
yanaesher <senyukyana26@gmail.com>
2025-09-25 10:09:57 +0200

    refac: move config to seperate file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getTypeOrmConfig } from './config/typeorm.config';
 import { MovieModule } from './movie/movie.module';
 import { TaskModule } from './task/task.module';
 
@@ -11,16 +12,8 @@ import { TaskModule } from './task/task.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.getOrThrow('POSTGRES_HOST'),
-        port: configService.getOrThrow('POSTGRES_PORT'),
-        username: configService.getOrThrow('POSTGRES_USER'),
-        password: configService.getOrThrow('POSTGRES_PASSWORD'),
-        database: configService.getOrThrow('POSTGRES_DB'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      imports: [ConfigModule],
+      useFactory: getTypeOrmConfig,
     }),
     TaskModule,
     MovieModule,
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from '@nestjs/config';
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function getTypeOrmConfig(
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> {
+  return {
+    type: 'postgres',
+    host: configService.getOrThrow('POSTGRES_HOST'),
+    port: configService.getOrThrow('POSTGRES_PORT'),
+    username: configService.getOrThrow('POSTGRES_USER'),
+    password: configService.getOrThrow('POSTGRES_PASSWORD'),
+    database: configService.getOrThrow('POSTGRES_DB'),
+    autoLoadEntities: true,
+    synchronize: true,
+  };
+}
