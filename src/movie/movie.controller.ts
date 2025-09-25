e241e323dbe5b28f97478eb053679c41d01c93e1
yanaesher <senyukyana26@gmail.com>
2025-09-25 07:57:37 +0200

    config database

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { MovieService } from './movie.service';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 
-@Controller('movie')
+@Controller({ path: 'movies' })
 export class MovieController {
-  constructor(private readonly movieService: MovieService) {}
+  @Get()
+  findAll(@Query('genre') genre: any) {
+    return genre ? `Films in genre ${genre}` : [{ title: 'fight club' }];
+  }
+
+  @Post()
+  create(@Body() body: { title: string; genre: string }) {
+    return `Movie "${body.title}" was added`;
+  }
 }
